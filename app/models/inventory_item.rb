c83c9651dfc83b7df354a21a59b73765f3090339
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-05-14 12:50:10 -0600

    Add numericality > 0 validations for price and inventory_count

--- app/models/inventory_item.rb
+++ app/models/inventory_item.rb
@@ -3,5 +3,6 @@ class InventoryItem < ApplicationRecord
   has_many :shipment_inventory_items
   has_many :shipments, through: :shipment_inventory_items
   validates :name, :description, :inventory_count, presence: true
-  validates :price, numericality: { only_integer: true }
+  validates_numericality_of :inventory_count, greater_than: 0
+  validates_numericality_of :price, greater_than: 0
 end
--- spec/models/inventory_item_spec.rb
+++ spec/models/inventory_item_spec.rb
@@ -10,6 +10,9 @@ RSpec.describe InventoryItem, type: :model do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:description) }
     it { should validate_presence_of(:inventory_count) }
+    it { should validate_numericality_of(:inventory_count).is_greater_than(0)}
     it { should validate_numericality_of(:price) }
+    it { should validate_numericality_of(:inventory_count) }
+    it { should validate_numericality_of(:price).is_greater_than(0)}
   end
 end
