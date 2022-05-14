664dd0997a782dc4c4ea3a457a0582932e49eeb9
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-05-14 12:41:28 -0600

    Add model with validations and relationships; tests pass

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -1,2 +1,6 @@
 class Shipment < ApplicationRecord
+  has_many :shipment_inventory_items
+  has_many :inventory_items, through: :shipment_inventory_items
+  validates :customer_name, presence: true
+  validates :customer_address, presence: true
 end
--- spec/models/shipment_spec.rb
+++ spec/models/shipment_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Shipment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "relationships" do
+    it { should have_many(:inventory_items).through(:shipment_inventory_items) }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of(:customer_name) }
+    it { should validate_presence_of(:customer_address) }
+  end
 end
