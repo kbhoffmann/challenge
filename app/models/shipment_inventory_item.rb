13ed57ca4f18879c3385933dd3456467b34d9023
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-05-14 12:52:48 -0600

    Add numericality > 0 validation for inventory_item_quantity

--- app/models/shipment_inventory_item.rb
+++ app/models/shipment_inventory_item.rb
@@ -1,5 +1,5 @@
 class ShipmentInventoryItem < ApplicationRecord
   belongs_to :inventory_item
   belongs_to :shipment
-  validates :inventory_item_quantity, presence: true, numericality: { only_integer: true }
+  validates :inventory_item_quantity, presence: true, numericality: { greater_than: 0 }
 end
--- spec/models/shipment_inventory_item_spec.rb
+++ spec/models/shipment_inventory_item_spec.rb
@@ -8,6 +8,6 @@ RSpec.describe ShipmentInventoryItem, type: :model do
 
   describe "validations" do
     it { should validate_presence_of(:inventory_item_quantity) }
-    it { should validate_numericality_of(:inventory_item_quantity) }
+    it { should validate_numericality_of(:inventory_item_quantity).is_greater_than(0)}
   end
 end
