f89c712015682fe620d2917ab456c843c9fb2edf
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-05-14 12:41:20 -0600

    Add model with validations and relationships; tests pass

--- app/models/inventory_item.rb
+++ app/models/inventory_item.rb
@@ -1,2 +1,7 @@
 class InventoryItem < ApplicationRecord
+  belongs_to :merchant
+  has_many :shipment_inventory_items
+  has_many :shipments, through: :shipment_inventory_items
+  validates :name, :description, :inventory_count, presence: true
+  validates :price, numericality: { only_integer: true }
 end
--- spec/models/inventory_item_spec.rb
+++ spec/models/inventory_item_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe InventoryItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "relationships" do
+    it { should belong_to :merchant }
+    it { should have_many(:shipments).through(:shipment_inventory_items) }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:inventory_count) }
+    it { should validate_numericality_of(:price) }
+  end
 end
