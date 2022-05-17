da56ace35f33ef60457b683f6f0e7a71edf97fc6
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-05-17 10:17:35 -0600

    Add :delete action to exception for inventory items

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get '/', to: 'welcome#index'
 
   resources :merchants, only: [:show] do
-    resources :inventory_items, except: [:delete]
+    resources :inventory_items, except: [:show, :delete]
     get '/inventory_items/:id/delete_item', to: 'inventory_items#delete_item_form'
     patch '/inventory_items/:id/delete', to: 'inventory_items#delete_item'
     patch '/inventory_items/:id/un_delete', to: 'inventory_items#un_delete_item'
